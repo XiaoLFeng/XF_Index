3162716710f44a2fd61d155fbcbf95868b9fb4bd
XiaoLFeng <gm@x-lf.cn>
2023-06-28 15:29:11 +0800

    更新链接，解决外网tailwindcss网速过慢问题

--- resources/views/modules/navbar.blade.php
+++ resources/views/modules/navbar.blade.php
@@ -80,7 +80,7 @@
             <div class="flex items-center justify-between">
                 <a href="#" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    <img class="h-8 w-auto" src="{{ asset('images/logo.jpg') }}"
                          alt="">
                 </a>
                 <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" onclick="openMenuOut()">
