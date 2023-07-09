8a52e35c945c3f7c7544a48e9f4b558ae6c85873
XiaoLFeng <gm@x-lf.cn>
2023-07-09 15:35:54 +0800

    AddFeature: 添加缺失处理办法
    添加 404.blade.php
    添加 no-permission.blade.php
    路由表配置对应数据及格式整理
    
    Signed-off-by: XiaoLFeng <gm@x-lf.cn>

--- routes/web.php
+++ routes/web.php
@@ -25,20 +25,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Index::class,'ViewIndex'])->name('home');
+Route::get('/', [Index::class, 'ViewIndex'])->name('home');
 Route::get('about', [Index::class, 'ViewAboutMe'])->name('about');
+Route::get('404', [Index::class, 'viewPageNotFounded'])->name('404');
+Route::get('no-permission', [Index::class, 'viewNoPermission'])->name('no-permission');
 Route::get('backup', [\App\Http\Controllers\DataBase::class, '__construct']);
 
 Route::prefix('function')->group(function () {
-    Route::get('link',[UserLink::class, 'viewLink'])->name('function.link');
+    Route::get('link', [UserLink::class, 'viewLink'])->name('function.link');
     Route::get('make-friend', [UserLink::class, 'viewMakeFriend'])->name('function.make-friend');
     Route::get('edit-search', [UserLink::class, 'viewSearchFriends'])->name('function.edit-search');
     Route::get('edit-search/{friendId}', [UserLink::class, 'viewSearchFriend'])->name('function.edit-searchOnly');
-    Route::get('edit-friend/{friendId}',[UserLink::class,'viewEditFriend'])->name('function.edit-friend');
-    Route::get('sponsor',function () {
+    Route::get('edit-friend/{friendId}', [UserLink::class, 'viewEditFriend'])->name('function.edit-friend');
+    Route::get('sponsor', function () {
         return view('function.sponsor');
     })->name('function.sponsor');
-    Route::get('music',function () {
+    Route::get('music', function () {
         return view('function.music');
     })->name('function.music');
 });
@@ -50,38 +52,38 @@ Route::prefix('account')->middleware('auth')->group(function () {
     });
 });
 
-Route::prefix('console')->middleware('auth')->group(function () {
-    Route::get('dashboard', [Dashboard::class,'ViewDashboard'])->name('console.dashboard');
+Route::prefix('console')->middleware('authConsole')->group(function () {
+    Route::get('dashboard', [Dashboard::class, 'ViewDashboard'])->name('console.dashboard');
     Route::prefix('friends-link')->group(function () {
-        Route::redirect('list','list/1');
-        Route::get('list',[ConsoleLink::class,'ViewList'])->name('console.friends-link.list');
-        Route::get('check',[ConsoleLink::class,'ViewCheck'])->name('console.friends-link.check');
-        Route::get('edit/{userId}',function ($userId) {
+        Route::redirect('list', 'list/1');
+        Route::get('list', [ConsoleLink::class, 'ViewList'])->name('console.friends-link.list');
+        Route::get('check', [ConsoleLink::class, 'ViewCheck'])->name('console.friends-link.check');
+        Route::get('edit/{userId}', function ($userId) {
             $ConsoleLink = new ConsoleLink();
             $request = new Request();
-            return $ConsoleLink->ViewEdit($request,$userId);
+            return $ConsoleLink->ViewEdit($request, $userId);
         })->name('console.friends-link.edit');
-        Route::get('add',[ConsoleLink::class,'ViewAdd'])->name('console.friends-link.add');
-        Route::get('sort',[ConsoleLink::class,'ViewSort'])->name('console.friends-link.sort');
-        Route::get('color',[ConsoleLink::class,'ViewColor'])->name('console.friends-link.color');
+        Route::get('add', [ConsoleLink::class, 'ViewAdd'])->name('console.friends-link.add');
+        Route::get('sort', [ConsoleLink::class, 'ViewSort'])->name('console.friends-link.sort');
+        Route::get('color', [ConsoleLink::class, 'ViewColor'])->name('console.friends-link.color');
     });
 });
 
 Route::prefix('auth')->group(function () {
-    Route::redirect('','auth/login');
+    Route::redirect('', 'auth/login');
     Route::get('login', function () {
         $data = (new Index())->data;
-        return view('auth.login',$data);
+        return view('auth.login', $data);
     })->name('login');
-    Route::get('register',function () {
+    Route::get('register', function () {
         $data = (new Index())->data;
-        return view('auth.register',$data);
+        return view('auth.register', $data);
     })->name('register');
-    Route::get('forgotpassword',function () {
+    Route::get('forgotpassword', function () {
         $data = (new Index())->data;
-        return view('auth.forgotpassword',$data);
+        return view('auth.forgotpassword', $data);
     })->name('forgotpassword');
-    Route::match(['get','post'],'logout',function () {
+    Route::match(['get', 'post'], 'logout', function () {
         Auth::logout();
         return Response::redirectTo('');
     })->name('logout');
