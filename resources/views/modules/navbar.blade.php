4c740708986dc3ed24db63a2bcdbe3ae82e70e73
XiaoLFeng <gm@x-lf.cn>
2023-07-23 11:27:14 +0800

    Modify: 常规修改
    添加基础图片显示，显示图内容，修改时区
    
    修改:
    - app.php
    - navbar.blade.php
    新增:
    - 256.png
    
    Signed-off-by: XiaoLFeng <gm@x-lf.cn>

--- resources/views/modules/navbar.blade.php
+++ resources/views/modules/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">Your Company</span>
-                <img class="h-8 w-auto rounded-full" src="{{ asset('images/logo.jpg') }}" alt="">
+                <img class="h-8 w-auto rounded-full" src="{{ $webIcon }}" alt="">
             </a>
         </div>
         <div class="flex lg:hidden">
